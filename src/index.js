ccfeb0b7c186d05f89ef0ff94841a54c63a93952
iulianstan <iulian1226@gmail.com>
2021-06-01 11:26:41 +0200

    src/index.js Update index.js, import sass and add comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// Import React and ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Import App.js
 import App from "./App";
+
+// Import SASS
+import "./sass/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
