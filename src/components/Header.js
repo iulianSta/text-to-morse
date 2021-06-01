3174853411be66010c8ee491ad13ca143b6db2a4
iulianstan <iulian1226@gmail.com>
2021-06-01 14:34:37 +0200

    src/components/Header.js Added Header.js and created Header function

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// Header function
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Please type your text to translate it in Morse code</h1>
+    </div>
+  );
+};
+
+export default Header;
