134f8fb9bc9085eca1d0adbf66665567f6a6d8fa
iulianstan <iulian1226@gmail.com>
2021-06-01 15:06:50 +0200

    src/components/Input.js Created Input function and export it

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,2 +1,22 @@
 // Import useState from "react"
 import { useState } from "react";
+
+// Input function
+const Input = (props) => {
+  const [userInput, setUserInput] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (userInput.length) {
+      props.setInput((prevState) => [...prevState, { text: userInput.trim() }]);
+      setUserInput("");
+    }
+  }
+  return (
+    <label>
+      <input type="textarea" value={userInput} onChange={handleSubmit} />
+    </label>
+  );
+};
+
+// Export default Input
+export default Input;
