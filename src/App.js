fc80a022de98305e88c8531baf5bc61018b012fd
iulianstan <iulian1226@gmail.com>
2021-06-01 14:41:01 +0200

    src/App.js Import Header.js in App.js and implement it in jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+// Import Header
+import Header from "./components/Header";
+
+// App function
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
