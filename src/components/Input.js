e704080c372d504a4bd6f877253890da057b9b9c
iulianstan <iulian1226@gmail.com>
2021-06-02 10:00:39 +0200

    src/components/Input.js Update Input function

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,14 @@
 // Import useState from "react"
-import { useState } from "react";
+// import { useState } from "react";
 
 // Input function
 const Input = (props) => {
-  const [userInput, setUserInput] = useState("");
-  //   console.log(userInput);
   function handleChange(e) {
     e.preventDefault();
   }
   return (
     <form>
-      <textarea onChange={handleChange} />
-
-      {/* <input type="text" value={userInput} onChange={handleChange} /> */}
+      <textarea type="text" onChange={handleChange} />
     </form>
   );
 };
